0251cedab671a391e27ad1caf42fd6f7714f4ed9
metamn <bartus.csongor@gmail.com>
2020-06-15 16:05:40 +0300

    Responsive images done

--- src/components/ImageResponsive/ImageResponsive.stories.js
+++ src/components/ImageResponsive/ImageResponsive.stories.js
@@ -13,8 +13,6 @@ export const Default = () => <ImageResponsive />;
 export const WithDefinedAspectRatio = () => (
   <ImageResponsive
     url="https://via.placeholder.com/480x270.png?text=480x270"
-    width={480}
-    height={270}
     aspectRatio={270 / 480}
     srcSet="https://via.placeholder.com/320x180.png?text=320x180 320w, https://via.placeholder.com/640x360.png?text=640x360 640w, https://via.placeholder.com/1280x720.png?text=1280x720 1280w"
     sizes="(max-width: 639px) 320px, (max-width: 1279px) 640px, 1280px"
