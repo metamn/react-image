8b31c2bc054c042a5bef40b89805e67975067060
metamn <bartus.csongor@gmail.com>
2020-06-23 15:44:46 +0300

    Rezising Aspect Ration Box with Responsive Image

--- src/components/ImageResponsive/ImageResponsive.stories.js
+++ src/components/ImageResponsive/ImageResponsive.stories.js
@@ -27,6 +27,28 @@ export const WithWidthAndHeight = () => (
     height={270}
     srcSet="https://via.placeholder.com/320x180.png?text=320x180 320w, https://via.placeholder.com/640x360.png?text=640x360 640w, https://via.placeholder.com/1280x720.png?text=1280x720 1280w"
     sizes="(max-width: 639px) 320px, (max-width: 1279px) 640px, 1280px"
-    boxStyle={{ backgroundColor: "red" }}
+    boxStyle={{
+      backgroundColor: "red"
+    }}
+  />
+);
+
+export const AspectRatioBoxIsSized = () => (
+  <ImageResponsive
+    url="https://via.placeholder.com/480x270.png?text=480x270"
+    aspectRatio={270 / 480}
+    srcSet="https://via.placeholder.com/320x180.png?text=320x180 320w, https://via.placeholder.com/640x360.png?text=640x360 640w, https://via.placeholder.com/1280x720.png?text=1280x720 1280w"
+    sizes="(max-width: 639px) 320px, (max-width: 1279px) 640px, 1280px"
+    boxStyle={{
+      backgroundColor: "red",
+      padding: 1,
+      width: 320,
+      ["@media (min-width: 640px)"]: {
+        width: 640
+      },
+      ["@media (min-width: 1280px)"]: {
+        width: 1280
+      }
+    }}
   />
 );
